57594d5532d7f00865015d9e89d6fd6b3112738b
Ravi <ravi.verma278@gmail.com>
2023-10-13 00:50:40 +0530

    reversestring

---
+++ string/reverseString.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class ReverseString:
+    def reverse(self,str1:str)->str:
+        str2=""
+        #print(str1)
+        for i in str1:
+            str2=i+str2
+            #print(i)
+        return str2
+
+
+if __name__ == '__main__':
+        abc = ReverseString()
+        print(abc.reverse("ravi"))
