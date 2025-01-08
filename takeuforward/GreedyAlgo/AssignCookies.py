3b5f305cbf693b4c3b5cac8a896a2c69ccfab858
Ravi <ravi.verma278@gmail.com>
2025-01-09 00:04:52 +0530

    GredyAlgo AssignCookies

--- takeuforward/GreedyAlgo/AssignCookies.py
+++ takeuforward/GreedyAlgo/AssignCookies.py
@@ -1,9 +1,20 @@
-class Solution:
-    from typing import List
+"""
+Problem Statement: Given two arrays representing children’s green factor and
+cookie sizes, the goal is to maximise the number of content children.
 
+Each child i has a greed factor of g[i],
+which is the minimum size of a cookie that will make the child content.
+Each cookie j has a size of s[j]. If s[j] >= g[j], we can assign cookie j
+to child i, making the child content. Each child can only receive one cookie.
+
+Link:
+https://takeuforward.org/Greedy/assign-cookies
+"""
+from typing import List
+class Solution:
     # Function to find the maximum
     # number of content children
-    def findContentChildren(greed: List[int], cookieSize: List[int]) -> int:
+    def findContentChildren(self,greed: List[int], cookieSize: List[int]) -> int:
         # Get the size of
         # the greed array
         n = len(greed)
@@ -51,20 +62,21 @@ class Solution:
         # of children that were satisfied
         return r
 
-    if __name__ == "__main__":
-        greed = [1, 5, 3, 3, 4]
-        cookieSize = [4, 2, 1, 2, 1, 3]
+if __name__ == "__main__":
+    greed = [1, 5, 3, 3, 4]
+    cookieSize = [4, 2, 1, 2, 1, 3]
 
-        print("Array Representing Greed: ", end="")
-        for g in greed:
-            print(g, end=" ")
-        print()
+    print("Array Representing Greed: ", end="")
+    for g in greed:
+        print(g, end=" ")
+    print()
 
-        print("Array Representing Cookie Size: ", end="")
-        for c in cookieSize:
-            print(c, end=" ")
-        print()
+    print("Array Representing Cookie Size: ", end="")
+    for c in cookieSize:
+        print(c, end=" ")
+    print()
 
-        ans = findContentChildren(greed, cookieSize)
+    obj=Solution()
+    ans = obj.findContentChildren(greed, cookieSize)
 
-        print(f"\nNo. of kids assigned cookies {ans}")
+    print(f"\nNo. of kids assigned cookies {ans}")
