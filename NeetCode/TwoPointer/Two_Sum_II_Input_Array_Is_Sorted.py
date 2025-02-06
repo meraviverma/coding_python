a8b2753c26c5d28263f3cf825d373b1c3385543e
Ravi <ravi.verma278@gmail.com>
2025-02-06 23:42:37 +0530

    Two sum input

--- NeetCode/TwoPointer/Two_Sum_II_Input_Array_Is_Sorted.py
+++ NeetCode/TwoPointer/Two_Sum_II_Input_Array_Is_Sorted.py
@@ -14,14 +14,49 @@ Input: numbers = [1,2,3,4], target = 3
 Output: [1,2]
 
 """
-
+from typing import List
 class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+    #Brute Force
+    def twoSumBruteForce(self, numbers: List[int], target: int) -> List[int]:
         for i in range(len(numbers)):
             for j in range(i + 1, len(numbers)):
                 if numbers[i] + numbers[j] == target:
                     return [i + 1, j + 1]
         return []
 
+    #BinarySearch
+    def twoSumBinarySearch(self, numbers: List[int], target: int) -> List[int]:
+        for i in range(len(numbers)):
+            l, r = i + 1, len(numbers) - 1
+            tmp = target - numbers[i]
+            while l <= r:
+                mid = l + (r - l) // 2
+                if numbers[mid] == tmp:
+                    return [i + 1, mid + 1]
+                elif numbers[mid] < tmp:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return []
+
+    def twoSum_twopointer(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers) - 1
+
+        while l < r:
+            curSum = numbers[l] + numbers[r]
+
+            if curSum > target:
+                r -= 1
+            elif curSum < target:
+                l += 1
+            else:
+                return [l + 1, r + 1]
+        return []
+
 if __name__=="__main__":
-    obj=
+    obj= Solution()
+    input=[1,2,3,4]
+    target=3
+    print(obj.twoSumBruteForce(input,target))
+    print(obj.twoSumBinarySearch(input, target))
+    print(obj.twoSum_twopointer(input, target))
