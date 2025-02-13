d1c35a6d3aa6bc4cc445282e6d8120e376325b71
Ravi <ravi.verma278@gmail.com>
2025-02-13 23:38:40 +0530

    quick sort

--- AlgorithmAndDataStructure/SearchingSorting/QuickSort.py
+++ AlgorithmAndDataStructure/SearchingSorting/QuickSort.py
@@ -10,6 +10,28 @@ def quick_srt(arr):
 
     return quick_srt(left) + middle + quick_srt(right)
 
+def swap(arr,i,j):
+    arr[i],arr[j]=arr[j],arr[i]
+    
+def partition(arr,low,high):
+
+    #choose pivot
+    pivot=arr[high]
+
+    #Index the smaller element and indicates
+    #the right position of pivot found so far
+    i = low - 1
+
+    for j in range(low,high):
+        if arr[j] < pivot:
+            i += 1
+            swap(arr,i,j)
+
+    swap(arr,i+1,high)
+    return i + 1
+
+
 if __name__=="__main__":
     arr=[3,6,8,10,1,2,1]
     print("Sorted Array Is : ",quick_srt(arr))
+
