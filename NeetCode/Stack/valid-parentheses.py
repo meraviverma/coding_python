449f977a924dbc0e5c1575098d3d711c0f1609b4
Ravi <ravi.verma278@gmail.com>
2025-03-13 23:57:44 +0530

    Valid Paranthesis

---
+++ NeetCode/Stack/valid-parentheses.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        while '()' in s or '{}' in s or '[]' in s:
+            s = s.replace('()', '')
+            s = s.replace('{}', '')
+            s = s.replace('[]', '')
+        return s == ''
+
+if __name__=="__main__":
+    s = "([{}])"
+    myobj=Solution()
+    myobj.isValid(s)
