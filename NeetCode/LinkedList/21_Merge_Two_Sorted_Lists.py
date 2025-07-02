638ef6b436559529ffb1810e0b53b31324ee25a0
Ravi <ravi.verma278@gmail.com>
2025-07-02 23:58:11 +0530

    merge two list

--- NeetCode/LinkedList/21_Merge_Two_Sorted_Lists.py
+++ NeetCode/LinkedList/21_Merge_Two_Sorted_Lists.py
@@ -1,80 +1,57 @@
-"""
-You are given the heads of two sorted linked lists list1 and list2.
-
-Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
-
-Return the head of the merged linked list.
-
-Example 1:
-Input: list1 = [1,2,4], list2 = [1,3,4]
-Output: [1,1,2,3,4,4]
-
-Example 2:
-
-Input: list1 = [], list2 = []
-Output: []
-
-Example 3:
-
-Input: list1 = [], list2 = [0]
-Output: [0]
-"""
 from typing import Optional
 
-
 class ListNode:
-    def __init__(self,val):
-        self.val=val
-        self.next=None
+    def __init__(self, val: int):
+        self.val = val
+        self.next = None
+
 class LinkList:
     def __init__(self):
-        self.head=ListNode(-1)
-        self.tail=self.head
-    def insertEnd(self,val):
-        self.tail.next=ListNode(val)
-        self.tail=self.tail.next
-    #You're telling Python not to move to the next line after printing.
-    # That way, all the values in the linked list appear side by side on the same line like:
+        self.head = ListNode(-1)  # dummy head
+        self.tail = self.head
+
+    def insertEnd(self, val: int):
+        self.tail.next = ListNode(val)
+        self.tail = self.tail.next
+
     def print(self):
-        curr=self.head.next
+        curr = self.head.next
         while curr:
-            print(curr.val,"--->",end="") #In Python, the print() function automatically adds a newline (\n) at
-            # the end of each output by default.
-            # But with end="", you're overriding that behavior. In your line:
-            curr=curr.next
+            print(curr.val, "--->", end="")
+            curr = curr.next
         print()
 
-    def mergeTwoLists(self,list1:Optional[ListNode],list2:Optional[ListNode])-> Optional[ListNode]:
-        if list1 is None:
-            return list2
-        if list2 in None:
-            return list2
-        if list1.val <= list2.val:
-            list1.next=self.mergeTwoLists(list1.next,list2)
-            return list1
-        else:
-            list2.next=self.mergeTwoLists(list1,list2.next)
-            return list2
+def mergeTwoLists(list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+    if list1 is None:
+        return list2
+    if list2 is None:
+        return list1
 
+    if list1.val <= list2.val:
+        list1.next = mergeTwoLists(list1.next, list2)
+        return list1
+    else:
+        list2.next = mergeTwoLists(list1, list2.next)
+        return list2
 
-if __name__=="__main__":
-    obj=LinkList()
-    obj.insertEnd(1)
-    obj.insertEnd(2)
-    obj.insertEnd(4)
+def printList(head: Optional[ListNode]):
+    curr = head
+    while curr:
+        print(curr.val, "--->", end="")
+        curr = curr.next
+    print()
 
-    obj.print()
+if __name__ == "__main__":
+    obj1 = LinkList()
+    for val in [1, 2, 4]:
+        obj1.insertEnd(val)
+    obj1.print()
 
-    obj2=LinkList()
-    obj2.insertEnd(1)
-    obj2.insertEnd(3)
-    obj2.insertEnd(4)
-    obj2.insertEnd(9)
+    obj2 = LinkList()
+    for val in [1, 3, 4, 9]:
+        obj2.insertEnd(val)
     obj2.print()
 
-    abc=LinkList()
-    mylist=abc.mergeTwoLists(obj,obj2)
-
-
-
-
+    mergedHead = mergeTwoLists(obj1.head.next, obj2.head.next)
+    print("Merged List:")
+    printList(mergedHead)
