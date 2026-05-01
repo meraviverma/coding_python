483553a31f84fcfeaa8c49236e19e06047b7df27
Ravi <ravi.verma278@gmail.com>
2026-05-01 21:12:22 +0530

    LargestRectangle

---
+++ NeetCode/Stack/LargestRectangle.py
@@ -0,0 +1,4 @@
+# You are given an array of integers heights where heights[i] represents the height of a bar.
+# The width of each bar is 1.
+#
+# Return the area of the largest rectangle that can be formed among the bars.
