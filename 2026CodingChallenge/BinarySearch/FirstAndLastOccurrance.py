02439751d85aef7fee16fcbd12313ea87f49e099
Ravi <ravi.verma278@gmail.com>
2026-06-21 00:09:16 +0530

    firstlastoccurance

---
+++ 2026CodingChallenge/BinarySearch/FirstAndLastOccurrance.py
@@ -0,0 +1,17 @@
+# Problem Statement: Given a sorted array of N integers, write a program to find the index of the last occurrence of the target key. If the target is not found then return -1. Note: Consider 0 based indexing
+
+# Example 1:
+# Input:
+#  N = 7, target = 13, array[] = {3, 4, 13, 13, 13, 20, 40}  
+# Output:
+#  4  
+# Explanation:
+#  The target value 13 appears for the first time at index number 2 in the array.  
+
+# Example 2:
+# Input:
+#  N = 7, target = 60, array[] = {3, 4, 13, 13, 13, 20, 40}  
+# Output:
+#  -1  
+# Explanation:
+#  Target value 60 is not present in the array, so the output is -1.
